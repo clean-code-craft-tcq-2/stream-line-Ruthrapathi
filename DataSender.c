f448c580cd96593eb8d03fc60a667714fe9f91a7
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-04-06 00:08:03 +0530

    Update DataSender.c

--- DataSender.c
+++ DataSender.c
@@ -3,6 +3,26 @@
 
 typedef struct
 {
-  float Temp;
+  float Temperature;
   float Soc;
 }BatParam_test;
+
+BatParam_test BatParam_val; 
+
+void SendOperation(BatParam_val)
+{
+    printf("{\"Temperature\":%0.2f,\"SOC\":%0.2f}\n",BatParam_val.Temperature,BatParam_val.Soc); // print in the console
+    
+}
+
+int main()
+{
+    for(int i=0;i<20;i++)
+    {
+        BatParam_val.Temperature = (float)i;
+        BatParam_val.Soc = (float)(i*2);
+        
+        SendOperation(BatParam_val);        
+    }
+    return 0;
+}
