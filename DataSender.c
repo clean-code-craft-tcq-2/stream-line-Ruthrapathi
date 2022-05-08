a300b203f2883e45b92103d2a45ad6eabd562de9
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-05-08 19:48:26 +0530

    Update DataSender.c

--- DataSender.c
+++ DataSender.c
@@ -1,21 +1,88 @@
-
+/*--------------------------------------------------------------------------------
+---    \file       BMS_Sender.c
+---   \author      Ruthrapathi 
+---
+---     \brief     proj file to stream BMS data
+------------------------------------------------------------------------------------*/
+/*------ standard includes -------*/
 #include <stdio.h>
-#include "DataSender.h"
+#include <stdlib.h>
 
-void SendOperation(BatParam_val)
+/*------ Project includes -------*/
+#include "BMS_Sender.h"
+
+/*Intial states of operation*/
+streamAlert_Status_s streamAlertStat = {NOT_SENT,FILE_ACCESS_FAILURE};
+
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    streamFileInpData
+ */
+/*!    \brief        read input data from file and stream continuosly
+ * 
+ *     \param       None
+ *     \returns     streamAlert_Status_s -success/fail
+ *
+*//*------------------------------------------------------------------------*/
+streamAlert_Status_s  streamFileInpData(char *inpFilePath)
 {
-    printf("{\"Temperature\":%0.2f,\"SOC\":%0.2f}\n",BatParam_val.Temperature,BatParam_val.Soc); // print in the console
-    
+  FILE *fptr;
+  unsigned int dataIndex_ui;
+  float tempRead ,chargerateRead;
+  /*Read the input file*/
+  fptr =fopen(inpFilePath,"r");
+
+  if ( NULL != fptr) /*validating file access*/
+  { 
+     /*msg that file access is success*/
+     streamAlertStat.FileReadStatus = FILE_ACCESS_SUCCESS; 
+     printf("Data Read from file as follows\n");
+     for(dataIndex_ui=0;fscanf(fptr,"%f\t%f", &tempRead,&chargerateRead) != EOF;dataIndex_ui++)
+     {
+       /*Req to print the data read from file in console*/
+       sendDataToConsole(tempRead,chargerateRead);
+     }
+     /*Close the file*/
+     fclose(fptr);
+     
+  }
+  else
+  {
+      printf("File access error and No output in console\n"); 
+      streamAlertStat.FileReadStatus = FILE_ACCESS_FAILURE; 
+      streamAlertStat.ConsoleSentStatus = NOT_SENT;
+  }  
+ 
+   return streamAlertStat;;
+ }  
+
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    sendDataToConsole
+ */
+/*!    \brief       send the data read from file continuosly
+ * 
+ *     \param        temp and chargerate
+ *     \returns     streamAlert_Status_s -
+ *
+*//*------------------------------------------------------------------------*/
+streamAlert_Status_s  sendDataToConsole(float temp ,float chargeRate)
+{
+   /*Prints temp and charge rate that read from file*/ 
+   printf("%5.2f;%5.2f\n",temp,chargeRate);
+   /*Print mgs status*/  
+   streamAlertStat.ConsoleSentStatus = SENT_TO_CONSOLE;
+   return streamAlertStat; 
 }
 
-int main()
+/*In current project stream sent from UT file 
+      if UT is disbaled then main shall be invoked (based on user choice) 
+*/
+#ifndef UNIT_TESTING
+int  main()
 {
-    for(int i=0;i<20;i++)
-    {
-        BatParam_val.Temperature = (float)i;
-        BatParam_val.Soc = (float)(i*2);
-        
-        SendOperation(BatParam_val);        
-    }
-    return 0;
+  /*File path used for streaming*/
+  char inpFilePath[50] = "Sender/Inputdata.txt";
+  /* BMS data stream request from file mentioned above*/
+  streamFileInpData(inpFilePath);
+  return 0;
 }
+#endif
