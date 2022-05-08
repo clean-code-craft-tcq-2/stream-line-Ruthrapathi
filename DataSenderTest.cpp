a6c26eaaff0c7520206be41f5f3089698e59cbb7
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-05-08 21:23:46 +0530

    Update DataSenderTest.cpp

--- DataSenderTest.cpp
+++ DataSenderTest.cpp
@@ -7,7 +7,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "../test/catch.hpp"
-#include "BMS_Sender.h"
+#include "DataSender.h"
 
 TEST_CASE("Test case 1 : Console output sent status") 
 {
