dc03e7a7aa557a6d17961ec19153871cd2119afd
Ruthrapathi <90901003+Ruthrapathi@users.noreply.github.com>
2022-05-09 09:12:27 +0530

    Update DataSenderTest.cpp

--- DataSenderTest.cpp
+++ DataSenderTest.cpp
@@ -20,3 +20,44 @@ TEST_CASE("Test case 1 : Console output sent status")
     REQUIRE(streamAlertTestStat.ConsoleSentStatus == SENT_TO_CONSOLE);
 
 }
+
+TEST_CASE("Test case 2 : validate file read and stream data with invalid  path") 
+{
+    /*Inital conditions*/	
+    streamAlert_Status_s streamAlertTestStat = {SENT_TO_CONSOLE,FILE_ACCESS_SUCCESS};
+    /*stub data*/ 	
+    char testFilePath[50] = "Inputdata.txt";
+    /*func call with invalid file path*/
+    streamAlertTestStat = streamFileInpData(testFilePath);
+	
+     REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_FAILURE);
+     REQUIRE(streamAlertTestStat.ConsoleSentStatus   == NOT_SENT);
+
+}
+
+TEST_CASE("Test case 3 : validate file read and stream data with invalid file name") 
+{
+    /*Inital conditions*/	
+    streamAlert_Status_s streamAlertTestStat = {SENT_TO_CONSOLE,FILE_ACCESS_SUCCESS};
+    /*stub data*/ 	
+    char testFilePath[50] = "data.txt";
+    /*func call with invalid file name*/
+    streamAlertTestStat = streamFileInpData(testFilePath);
+	
+     REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_FAILURE);
+     REQUIRE(streamAlertTestStat.ConsoleSentStatus   == NOT_SENT);
+}
+
+
+TEST_CASE("Test case 4 : validate that file read and stream data with valid file") 
+{
+    /*Inital conditions*/	
+    streamAlert_Status_s streamAlertTestStat = {NOT_SENT,FILE_ACCESS_FAILURE};
+    /*stub data*/ 	
+    char testFilePath[50] = "Inputdata.txt";
+    /*func call with valid file name*/
+    streamAlertTestStat = streamFileInpData(testFilePath);
+	
+    REQUIRE(streamAlertTestStat.FileReadStatus   == FILE_ACCESS_SUCCESS);
+    REQUIRE(streamAlertTestStat.ConsoleSentStatus   == SENT_TO_CONSOLE);
+}
